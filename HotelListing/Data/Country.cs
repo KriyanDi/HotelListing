42dc810ebe53cc92e386ef8122bcf8204b31c3ba
kriyandi <kriyandimitrov@gmail.com>
2021-08-06 13:19:14 +0300

    Added Database, AutoMapper, UnitOfWork, DTO

---
+++ HotelListing/Data/Country.cs
@@ -0,0 +1,17 @@
+﻿using HotelListing.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing.Data
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ShorName { get; set; }
+
+        public virtual IList<Hotel> Hotels { get; set; }
+    }
+}
